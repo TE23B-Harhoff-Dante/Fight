6ee789baa122d49ad7d63e1f7185c12750e52ba5
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-09-24 13:25:25 +0200

    xfhbxgff

--- Fight/Program.cs
+++ Fight/Program.cs
@@ -1,1 +1,41 @@
-﻿int hp = 100;
+﻿
+using System.Xml.Serialization;
+
+int hp1 = Random.Shared.Next(80, 101);
+int hp2 = Random.Shared.Next(80, 101);
+
+int skada = Random.Shared.Next(0, 21);
+string choice = "";
+
+
+Console.WriteLine("Skriv ditt namn");
+String name = Console.ReadLine();
+
+while (name == "")
+{
+    Console.WriteLine("bruh");
+    Console.WriteLine("försök igen");
+    name = Console.ReadLine();
+}
+
+Console.WriteLine($"{name} {hp1}hp VS Demon {hp2}hp");
+Console.WriteLine("Skriv a för att göra 0-20 skada");
+
+choice = Console.ReadLine();
+
+while (hp1 > 0 && hp2 > 0)
+{
+
+    if (choice.ToLower() == "a")
+        hp1 -= skada;
+
+
+    hp2 -= skada;
+
+    Console.WriteLine($"{name} har kvar {hp1}hp och Demon har kvar {hp2}hp");
+    Console.ReadLine();
+
+}
+
+Console.ReadLine();
+
