7ea666f4dffa75cb26cf96436f279f0f364d6225
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-09-25 14:59:40 +0200

    dthdsdgh

--- Fight/Program.cs
+++ Fight/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Threading.Tasks.Dataflow;
 using System.Xml.Serialization;
 
 int hp1 = Random.Shared.Next(80, 101);
@@ -18,7 +19,7 @@ while (name == "")
     name = Console.ReadLine();
 }
 
-Console.WriteLine($"{name} {hp1}hp VS Demon {hp2}hp");
+Console.WriteLine($"{name} {hp1}hp VS {name}killer {hp2}hp");
 Console.WriteLine("Skriv a för att göra 0-20 skada");
 
 choice = Console.ReadLine();
@@ -27,15 +28,36 @@ while (hp1 > 0 && hp2 > 0)
 {
 
     if (choice.ToLower() == "a")
-        hp1 -= skada;
+        skada = Random.Shared.Next(0, 21);
 
+    Console.WriteLine($"{name} gjorde {skada} skada");
 
+    hp1 -= skada;
+    skada = Random.Shared.Next(0, 21);
     hp2 -= skada;
 
-    Console.WriteLine($"{name} har kvar {hp1}hp och Demon har kvar {hp2}hp");
-    Console.ReadLine();
+    Console.WriteLine($"{name}killer gjorde {skada} skada");
 
+    Console.WriteLine($"{name} har kvar {hp1}hp och {name}killer har kvar {hp2}hp");
+    Console.ReadLine();
 }
+
+if (hp1 > 0)
+{
+    Console.WriteLine(" YOU WIN");
+}
+
+else if (hp2 > 0)
+{
+    Console.WriteLine(" YOU LOSE");
+}
+else
+{
+    Console.WriteLine("DRAW");
+}
+
+
+
 
 Console.ReadLine();
 
